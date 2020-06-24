ff9343a724c33dac9f24508bdd3814631e36d025
Mucheli <461721@cognizant.com>
2020-06-24 21:42:34 +0530

    Updates

---
+++ precache-manifest.34ef295e27de949123e26b86b7ab96bf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8025288f68222ebae3f9b810ea8e9ed3",
+    "url": "/my-app-one/index.html"
+  },
+  {
+    "revision": "7de790d22655b5378b11",
+    "url": "/my-app-one/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "e629b0fa28c4bed310dc",
+    "url": "/my-app-one/static/js/2.f4d45514.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/my-app-one/static/js/2.f4d45514.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7de790d22655b5378b11",
+    "url": "/my-app-one/static/js/main.13587437.chunk.js"
+  },
+  {
+    "revision": "17edfd09108bc2390870",
+    "url": "/my-app-one/static/js/runtime-main.ad4029b0.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/my-app-one/static/media/logo.5d5d9eef.svg"
+  }
+]);
